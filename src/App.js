609335782617620084e898792e80a199332e15ba
mmichaud3 <51766819+mmichaud3@users.noreply.github.com>
2023-04-16 16:04:47 -0400

    projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import projects from './projects';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [section, setSection] = useState('projects');
+
+  const nav = (
+    <Navbar sticky='top' bg='light' expand='lg'>
+      <Container>
+        <Nav.Link onClick={() => setSection('projects')}>
+          Mahelet Steinmetz
+        </Nav.Link>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link onClick={() => setSection('about')}>About</Nav.Link>
+            <Nav.Link onClick={() => setSection('projects')}>Projects</Nav.Link>
+            <Nav.Link onClick={() => setSection('contact')}>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+
+  const projectCards = (
+    <>
+      <Row xs={1} md={2} className='g-4'>
+        {projects.map((el) => (
+          <Col>
+            <Card className='bg-dark text-white project-card'>
+              <Card.Img
+                src={el.image}
+                alt='Card image'
+                className='project-card__image'
+              />
+              <Card.ImgOverlay className='project-card__overlay'>
+                <Card.Title>{el.title}</Card.Title>
+                <Card.Text>{el.text}</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <>
+        {nav}
+        <Container>
+          {section === 'projects' && (
+            <>
+              {' '}
+              <div className='heading'>
+                <h1 className='heading__name'>Mahelet Steinmetz</h1>
+                <h3>Graphic Design Major at The University of Connecticut</h3>
+              </div>
+              <div>{projectCards}</div>
+            </>
+          )}
+        </Container>
+      </>
     </div>
   );
 }
---
+++ src/projects.js
@@ -0,0 +1,106 @@
+import magicPotluck from './images/magic-potluck.png';
+import risoPoster from './images/Riso_poster.png';
+import typeFace from './images/typeface.png';
+import fashion from './images/fashion.png';
+import poster from './images/poster.png';
+import spotify from './images/spotify.png';
+import project3 from './images/project3.png';
+import wiki from './images/wiki.png';
+import soccer from './images/soccer.png';
+import stamp from './images/stamp.png';
+
+export const projects = [
+  {
+    title: 'Graphic Design 2: Visual Systems',
+    image: magicPotluck,
+    text: `For this brief, we had to design and hold our events. This event can be anything
+    you want it to be but it has to be a 'specific, unique, and disputable purpose'
+    (Priya Parker, The Art of Gathering). We had to then create a visual system for
+    our event which could be a color palette, a specific typography on theme with
+    your event, and other components like an invitation, decorations for the event,
+    etc`,
+  },
+  {
+    title: 'Graphic Design 2: Visual Systems',
+    image: risoPoster,
+    text: `For this brief, we designed a flexible visual system for a fictional city or
+    “imaginary city” as Marco Polo describes in Italo Calvino's book, Invisible Cities.
+    First, each student chose a random city from Invisible Cities out of a hatful of city
+    names. Then we read the poem about our city and then began identifying words
+    in the poem that characterized our city the best. From those words, I also chose
+    images that corresponded as well as a color palette. My city was the City of
+    Isaura and I decided to focus on its abundance of water as described on page 20
+    of Calvinos, Invisible Cities. We then chose a phrase that best described our city
+    and then used all of these elements to create 2 shapes or components which
+    could be used to create a visual representation, in this case, a poster of our
+    cities. Finally, we used these poster designs to print out a Riso book showcasing
+    every student's visual system based on their city.`,
+  },
+  {
+    title: 'Graphic Design 1: Typography',
+    image: typeFace,
+    text: `For this project, create your own fonts based on an already existing one of our
+    choosing. I chose Baskerville because I liked the way the stroke changed from
+    thin and thick throughout the letterforms. We created our typeface by using the
+    pen tool in illustrator to trace the letters in the original font so we had a basis to
+    start from. Our typeface also had to fit within the alphabet seamlessly`,
+  },
+  {
+    title: 'Graphic Design 1: Typography',
+    image: fashion,
+    text: `We created an anthology that combined articles that we had picked based on our
+    chosen topic. I chose to research fashion in different cultures and so the images
+    and text throughout my anthology are from the articles I chose to work with. For
+    this anthology, we also had to create a grid that best represented our information.`,
+  },
+  {
+    title: 'Graphic Design 1: Typography',
+    image: poster,
+    text: ` Every year our department has a Bachelor of Fine Arts Show where the seniors
+    showcase their year-long study and art. So for this project, we created a specific
+    visual identity that would act as the basic theme for a poster design. The poster
+    had to have the name of the show, the time and place, and then creative aspects
+    that represented your theme or identity. Then we had to create a presentation to
+    present in front of the class and persuade them that our poster identity was the
+    best one. At the end, we voted for our favorite and the winner had their poster
+    showcased in the show.`,
+  },
+  {
+    title: 'Introduction to Web Design',
+    image: spotify,
+    text: `Project 1: There's already an app for that - redesign an app for a specific purpose.
+    My purpose was to create a user friendly spotify for older generations.`,
+  },
+  {
+    title: 'Introduction to Web Design',
+    image: wiki,
+    text: `Project 2: Make it Pop - Revamp an already existing website.
+    My task was to modernize Wikipedia for a more professional look.`,
+  },
+  {
+    title: 'Introduction to Web Design',
+    image: project3,
+    text: `Project 3: Design for Good - Create a Website that can help any disadvantaged group.
+   `,
+  },
+  {
+    title: 'Job: Uconn Nutmeg Publishing Fall 2022',
+    image: soccer,
+    text: `Student run group that creates yearbooks for the graduating seniors and a magazine
+    that spotlites UConn student life.
+    My job was to design spreads for the yearbook. Yearbook topics could be about sports or
+    Uconn clubs and organizations.
+   `,
+  },
+  {
+    title: 'Class: Graphic Design Process and Thinking Fall 2021',
+    image: stamp,
+    text: `This project was called typo texture and we had to create a pattern or texture out
+    of letters of different parts or parts of the letter we decided to focus on. We then
+    chose two Riso colors and printed them out in the Riso printer. The final product
+    was a typo-texture stamp which we exchanged with our classmates.
+   `,
+  },
+];
+
+export default projects;
